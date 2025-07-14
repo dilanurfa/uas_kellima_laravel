7ea7c56fd1e3bdc0fe9068749e23f124f50fdbf7
Nerschio <kanesya1221@gmail.com>
2025-07-14 15:04:35 +0700

    Update welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -183,7 +183,7 @@
             <p>Ayo booking sekarang juga, sebelum keduluan orang lain!</p>
           </div>
           <div class="col-xl-3 cta-btn-container text-center">
-            <a class="cta-btn align-middle" href="#">Booking</a>
+            <a class="cta-btn align-middle" href="login">Booking</a>
           </div>
         </div>
 
