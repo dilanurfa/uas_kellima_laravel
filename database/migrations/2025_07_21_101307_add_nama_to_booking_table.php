eb59c54775e94c49514255520adeb2b48f04e6ca
Dila Nurfadilah <dilanurfadilah275@gmail.com>
2025-07-24 21:37:07 +0700

    2025_07_21_101307_add_nama_to_booking_table

--- database/migrations/2025_07_21_101307_add_nama_to_booking_table.php
+++ database/migrations/2025_07_21_101307_add_nama_to_booking_table.php
@@ -6,20 +6,13 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('booking', function (Blueprint $table) {
-            // Tambahkan kolom nama, buat nullable supaya SQLite tidak error
             $table->string('nama', 100)->nullable()->after('ruangan_id');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('booking', function (Blueprint $table) {
